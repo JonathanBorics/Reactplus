21883ce43b94cc935fa1a63c36c4eb4fe49a3c5d
Jonathan <jonathanborics97@gmail.com>
2024-02-06 18:49:38 +0100

    first commit

---
+++ webplus/src/gyakorlas.js
@@ -0,0 +1,38 @@
+
+//++++++++ At here i learn about usestate++++++++++++
+
+// import React from 'react'
+// import { useState } from 'react';
+// const Content = () => {
+
+//   const [name,setname]= useState("Kaco");
+
+//     const Handle =()=>{
+//         const name =['Djulian','Kaco','Djimi'];
+//         const int = Math.floor(Math.random()*3);
+//         setname(name[int]);
+//       }
+//       const handleClick =()=>{
+//         console.log('you click it')
+//       }
+//       const handleClick2 =(name)=>{
+//         console.log(`${name} was clicked `)
+//       }
+//       const handleClick3 =(e)=>{
+//         console.log(e.target.innerText)
+//       }
+
+//   return (
+//     <main>
+//       <p>
+//         Helloo {name}!
+//         <button onClick={Handle}> click </button>
+//         <button onClick={() =>handleClick2('djulian')}> click it</button>
+//         <button onClick={(e)=>handleClick3(e)}> click it</button>
+//       </p>
+
+//     </main>
+//   )
+// }
+
+// export default Content
